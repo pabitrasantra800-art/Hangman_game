461f40dc976c02b9af4398a9813daf0322c080ed
pabitrasantra800-art <pabitrasantra800@gmail.com>
2026-01-05 18:54:28 +0530

    Update Hangman_game.py

--- Hangman_game.py
+++ Hangman_game.py
@@ -1,1 +1,21 @@
+import random
 
+words = ["apple", "train", "chair", "house", "plane"]
+
+word = random.choice(words)
+chances = 6
+
+print("Guess the word")
+
+while chances > 0:
+    guess = input("Enter your word: ")
+
+    if guess == word:
+        print("Correct! You won")
+        break
+    else:
+        chances -= 1
+        print("Wrong guess. Chances left:", chances)
+
+if chances == 0:
+    print("You lost. The word was:", word)
